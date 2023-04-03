d34ff9be2790f29ab0f9004efcb24477b2527651
bonraton <bonraton@yandex.ru>
2023-04-03 15:02:01 +0200

    implement hook form

--- src/routes/logged-out-router.tsx
+++ src/routes/logged-out-router.tsx
@@ -1,15 +1,56 @@
+import { useForm } from 'react-hook-form';
 import { isLoggedInVar } from '../apollo';
 
-export default function LoggedOutRouter() {
+interface IForm {
+  email: string;
+  password: string;
+}
 
-    const onClick = () => {
-        isLoggedInVar(true)
-    }
+export default function LoggedOutRouter() {
+  const onClick = () => {
+    isLoggedInVar(true);
+  };
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
+
+  const onSubmit = () => {
+    console.log(watch().email);
+  };
+
+  const onInvalid = () => {
+    console.log('ERROR');
+  };
+
+  console.log(errors);
 
   return (
     <div>
-      <h3 className='text-red-600 font-extrabold'>ЕСЛИ ЗАЛОГИНЕН</h3>
-      <button onClick={onClick} className='border-solid bg-clip-padding'>РАЗЛОГИНИТЬСЯ</button>
+      <h1>LoggedOut</h1>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            {...register('email', {
+              required: 'This is required',
+              validate: (email: string) => email.includes('@gmail.com'),
+            })}
+            placeholder='email'
+          />
+          {errors.email?.message && (
+            <span className='font-bold'>{errors.email.message}</span>
+          )}
+        </div>
+        <div>
+          <input
+            {...register('password', { required: true })}
+            placeholder='password'
+          />
+        </div>
+        <button type='submit'>BUTTON</button>
+      </form>
     </div>
   );
 }
